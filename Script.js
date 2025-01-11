3caa05706380553114aab718ee37909a5d3947fd
Saurav Anil Sultane <127301681+sauravsultane@users.noreply.github.com>
2025-01-11 23:55:41 +0530

    UserManager
    First Commit

---
+++ Script.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const userModel = require("./models/user")
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+
+app.get("/read",async(req,res)=>{
+    let users=await userModel.find()
+    res.render("read",{users});
+})
+
+app.get("/edit/:userid",async(req,res)=>{
+    let user=await userModel.findOne({_id:req.params.userid})
+    res.render("edit",{user});
+})
+app.post("/update/:userid",async(req,res)=>{
+    let{name,email,image}=req.body;
+    let user=await userModel.findOneAndUpdate({_id:req.params.userid},{name,email,image},{new:True})
+    res.redirect("/read");
+})
+
+
+app.post("/create",async(req,res)=>{
+
+    let{name,email,image}=req.body;
+    let createdUser=await userModel.create({
+        name,
+        email,
+        image
+
+    })
+
+    res.redirect("/read");
+})
+
+app.get("/delete/:id", async (req, res) => {
+    try {
+        let user = await userModel.findOneAndDelete({ _id: req.params.id });
+        if (user) {
+            console.log(`User with ID ${req.params.id} deleted.`);
+        } else {
+            console.log(`No user found with ID ${req.params.id}.`);
+        }
+        res.redirect("/read");
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).send("An error occurred while deleting the user.");
+    }
+});
+
+app.listen(3000);
